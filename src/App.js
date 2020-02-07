e119ab3574e7ea03ebeac087950514043263a21d
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-02-06 19:42:12 -0600

    add text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Travis will work?</p>
+        <p>changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
